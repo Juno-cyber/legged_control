4c6e6792bb82e5f69c3e1f29ec9df065651dfba9
juno-cyber <912783316@qq.com>
2025-04-17 01:17:34 +0800

    d5.1

--- launch_leg_sim.sh
+++ launch_leg_sim.sh
@@ -7,10 +7,10 @@ gnome-terminal --title="GAZEBO" -- bash -c \
   "echo '窗口1: Gazebo环境 (ROBOT_TYPE=$ROBOT_TYPE)'; 
    export ROBOT_TYPE=$ROBOT_TYPE; 
    source devel/setup.bash;
-   roslaunch legged_unitree_description empty_world.launch; 
+   roslaunch legged_unitree_description load_urdf.launch; 
    exec bash"
 
-sleep 1
+sleep 3
 
 # 窗口2：加载控制器
 gnome-terminal --title="CONTROLLER" -- bash -c \
@@ -21,8 +21,8 @@ gnome-terminal --title="CONTROLLER" -- bash -c \
    exec bash"
 
 # 窗口3：启动RQT界面
-gnome-terminal --title="RQT" -- bash -c \
-  "echo '窗口3: RQT控制界面（手动点击 Plugins > Robot Tools > Controller Manager）'; 
-   source devel/setup.bash;
-   rqt;
-   exec bash"
+# gnome-terminal --title="RQT" -- bash -c \
+#   "echo '窗口3: RQT控制界面（手动点击 Plugins > Robot Tools > Controller Manager）'; 
+#    source devel/setup.bash;
+#    rqt;
+#    exec bash"
